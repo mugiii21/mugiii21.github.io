2070fdc84d21a06f41ec4091e4255cc27f8eb6d2
mugiii21 <166462756+mugiii21@users.noreply.github.com>
2024-04-09 08:44:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const wrapper=document.querySelector(".wrapper");
+const question=document.querySelector(".question");
+const gif=document.querySelector(".gif");
+const yesBtn=document.querySelector(".yes-btn");
+const noBtn=document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "MAACIHH MWAAH";
+gif.src =
+"https://media1.tenor.com/m/LfVXeDuwInMAAAAC/dudububu-kiss.gif"; 
+});
+
